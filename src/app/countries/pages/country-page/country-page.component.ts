af0aed933a9af252c75a7bb4be6edc9549e67f52
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-03 13:29:17 +0200

    Navegar hacia la pagina del pais

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountryService } from '../../services/countries.service.ts.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -11,19 +12,23 @@ import { CountryService } from '../../services/countries.service.ts.service';
 })
 export class CountryPageComponent implements OnInit {
 
-  public countries: Country[] = [];
+  public country?: Country;
 
-  constructor(private activatedRoute: ActivatedRoute, private countryService: CountryService) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countryService: CountryService,
+    private router: Router) {
 
   }
   ngOnInit(): void {
     this.activatedRoute.params
-      .subscribe(({ id }) => {
-        this.countryService.searchCountryByAlphaCode(id)
-          .subscribe(country => {
-            console.log(country)
-          })
+      .pipe(
+        switchMap(({ id }) => this.countryService.searchCountryByAlphaCode(id))
+      )
+      .subscribe(country => {
+        if (!country) return this.router.navigateByUrl("")
+        return this.country = country
+
       })
   }
-
 }
