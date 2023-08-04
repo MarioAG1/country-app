b1889f1597592b840f8116375c78125df4330238
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-08-04 12:27:11 +0200

    Refactorizacion de codigo y delay

--- src/app/countries/services/countries.service.ts.service.ts
+++ src/app/countries/services/countries.service.ts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -10,38 +10,34 @@ export class CountryService {
 
   constructor(private http: HttpClient) { }
 
-  searchCapital(term: string): Observable<Country[]> {
-    const url = `${this.apiUrl}/capital/${term}`
-
-
+  private getHttpRequest(url: string): Observable<Country[]> {
     return this.http.get<Country[]>(url)
-      // Atrapar el error
       .pipe(
-        catchError(() => of([]))
+        catchError(() => of([])),
+        delay(2000)
       )
   }
+
+  searchCapital(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/capital/${term}`
+    return this.getHttpRequest(url)
+  }
+
   searchCountry(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${term}`
-
-    return this.http.get<Country[]>(url)
-      .pipe(
-        catchError(() => of([]))
-      )
+    return this.getHttpRequest(url)
 
   }
   searchRegion(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${term}`
-
-    return this.http.get<Country[]>(url)
-      .pipe(
-        catchError(() => of([]))
-      )
+    return this.getHttpRequest(url)
   }
+
   searchCountryByAlphaCode(code: string): Observable<Country | null> {
     const url = `${this.apiUrl}/alpha/${code}`
     return this.http.get<Country[]>(url)
       .pipe(
-        map (countries => countries.length > 0 ? countries[0]: null),
+        map(countries => countries.length > 0 ? countries[0] : null),
         catchError(() => of(null))
       )
 
